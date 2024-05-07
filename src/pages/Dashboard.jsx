ab059597204cb42ba7b6bcf614fc9da7fd8fd1be
themystic1 <cluckygo@gmail.com>
2024-05-07 06:39:43 +0100

    Static layout v1

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,15 @@
+import TopNav from "../components/dashboard/TopNav";
+import MainDashboard from "../components/dashboard/MainDashboard";
+import Footer from "../components/dashboard/Footer";
+
 function Dashboard() {
-  return <div></div>;
+  return (
+    <div className="ugo-dashboard">
+      <TopNav />
+      <MainDashboard />
+      <Footer />
+    </div>
+  );
 }
 
 export default Dashboard;
