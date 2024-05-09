5ec2a794aa91d654dce371c40499931ee9415a4f
themystic1 <cluckygo@gmail.com>
2024-05-09 19:02:26 +0100

    modified

--- src/components/Menu/MenuWrapper.jsx
+++ src/components/Menu/MenuWrapper.jsx
@@ -1,26 +1,28 @@
-import React from 'react'
-import BkArrow from '../GameLevel/img/bk-arrow.png'
+import React from "react";
+import BkArrow from "../GameLevel/img/bk-arrow.png";
+import { Link } from "react-router-dom";
 
 function MenuWrapper() {
   return (
     <div className="container">
-    <a href=""> <img src={BkArrow} alt="" /></a>
-        
-        <div className="settingWrapper">
-         
-         <div className="setting-content">
+      <Link to="">
+        {" "}
+        <img src={BkArrow} alt="" />
+      </Link>
+
+      <div className="settingWrapper">
+        <div className="setting-content">
           <h4>Menu</h4>
-         
-          <a href="Level">Single player mode</a>
-          <a href="Level">Play with friends</a>
-          <a href="LeaderBoard">Leaderboard</a>
-          <a href="">How to play</a>
-          <a href="Settings">Settings</a>
-         </div>
-    
+
+          <Link to="/level">Single player mode</Link>
+          <Link to="/level">Play with friends</Link>
+          <Link to="/leaderBoard">Leaderboard</Link>
+          <Link to="/how-to-play">How to play</Link>
+          <Link to="/settings">Settings</Link>
         </div>
-        </div>
-  )
+      </div>
+    </div>
+  );
 }
 
-export default MenuWrapper
+export default MenuWrapper;
