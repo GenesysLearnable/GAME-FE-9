e84fb094b6e4ded0d1d689fd0fc0de5a2787feb0
themystic1 <cluckygo@gmail.com>
2024-05-09 06:13:04 +0100

    modifying some components

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 82bc241 (modifying some components)
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+<<<<<<< HEAD
 import "./components/dashboard/dashboard.css";
 
 import "./components/GameLevel/Gamelevel.css";
@@ -21,9 +25,15 @@ import './components/GameLevel/Gamelevel.css'
 import './components/settings/Settings.css'
 import './components/Menu/Menu.css'
 >>>>>>> 35279cd (Revert "Static layout v1")
+=======
+import "./components/GameLevel/Gamelevel.css";
+import "./components/dashboard/dashboard.css";
+import "./components/settings/Settings.css";
+import "./components/Menu/Menu.css";
+>>>>>>> 82bc241 (modifying some components)
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,14 @@
+import MainDashboard from "../components/dashboard/MainDashboard";
+import TopNav from "../components/dashboard/TopNav";
+import Footer from "../components/dashboard/Footer";
+
 function Dashboard() {
   return (
+<<<<<<< HEAD
     <div className="ugo-dashboard">
+=======
+    <div>
+>>>>>>> 82bc241 (modifying some components)
       <TopNav />
       <MainDashboard />
       <Footer />
