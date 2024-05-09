82bc241c69101aab3d156b0f305ed73c9af1796b
themystic1 <cluckygo@gmail.com>
2024-05-09 06:13:04 +0100

    modifying some components

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,15 @@
+import MainDashboard from "../components/dashboard/MainDashboard";
+import TopNav from "../components/dashboard/TopNav";
+import Footer from "../components/dashboard/Footer";
+
 function Dashboard() {
-  return <div></div>;
+  return (
+    <div>
+      <TopNav />
+      <MainDashboard />
+      <Footer />
+    </div>
+  );
 }
 
 export default Dashboard;
