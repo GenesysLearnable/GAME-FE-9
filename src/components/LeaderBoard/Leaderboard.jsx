c4ecc8948c1f988515d208655c2f03a632213492
somilisa <somilisa1010@gmail.com>
2024-05-20 21:09:21 +0100

    feat: redesigned landing page

--- src/components/LeaderBoard/Leaderboard.jsx
+++ src/components/LeaderBoard/Leaderboard.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./styles.css";
 import { backButton, leaderboardStar } from "../../constants";
-// import LeaderboardItem from "./LeaderboardItem";
 import { data } from "./data";
 import LeaderboardItem from "./LeaderBoardItem";
 
 function Leaderboard() {
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1); // This takes you back to the previous page
+  };
   return (
     <div className="leaderboard">
-      <img src={backButton} alt="Go Back" />
+      <div className="back-button" onClick={goBack}>
+        <img src={backButton} alt="Go Back" />
+      </div>
+
       <div className="leaderboard__main">
         <header className="leaderboard__header">
           <h1 className="leaderboard__title">LEADERBOARD</h1>
